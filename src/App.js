d7c465d9bb3f17a6cb3f22a5e97200259385bc76
kamalesh babu <kamaleshbabu57@gmail.com>
2024-10-19 16:23:11 +0530

    Finished the task
    1.successfully fetching the users,posts,comments from the external link
    2.worked with API.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useState, useEffect } from 'react'
+import Form from './Form';
+import List from './List';
+const App = () => {
+  const API_URL="https://jsonplaceholder.typicode.com/";
+  const [reqType, setReqType]=useState('users');
+  const [items,setItems]= useState([]);
 
-function App() {
+  useEffect(()=>{
+    const fetchReq=async()=>{
+      try {
+        const res= await fetch(`${API_URL}${reqType}`);
+        if(!res.ok) throw new Error("there was an error")
+        const data=await res.json();
+        
+        setItems(data);
+        
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchReq();
+  },[reqType])
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Form 
+      reqType={reqType}
+      setReqType={setReqType}
+      />
+      <List
+      items={items}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
